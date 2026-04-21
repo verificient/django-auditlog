cdad01d6102819de3c5deb4479a2be508afabb98
Vitthal Mane <vitthal@verificient.com>
2026-04-21 23:17:03 +0530

    removed deprecated package - pkg_resources

--- auditlog/__init__.py
+++ auditlog/__init__.py
@@ -1,9 +1,10 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = get_distribution("django-auditlog").version
-except DistributionNotFound:
+    __version__ = version("django-auditlog")
+except PackageNotFoundError:
     # package is not installed
     pass
 
 default_app_config = "auditlog.apps.AuditlogConfig"
+
